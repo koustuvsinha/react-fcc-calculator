dda238b644ad0276aea6f5284682c19295ce7a04
Koustuv Sinha <koustuvsinha@hotmail.com>
2016-06-12 17:38:07 +0530

    Update webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,11 @@ module.exports = {
   },plugins: [
       new ExtractTextPlugin('style.css', {
           allChunks: true
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
       })
   ]
 };
